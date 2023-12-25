ccb7e1d76782346332a99cac0172d1aa8d781707
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-12-25 21:09:42 +0530

    Add feedback form layout and styles

--- src/Pages/Feedback/Feedback.js
+++ src/Pages/Feedback/Feedback.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './Feedback.css'
 
 const Feedback = () => {
   return (
-    <div>Feedback</div>
+    <>
+      <div className="feedback-block">
+        <h1 className='feedback'>Feedback</h1>  
+      </div>
+      <div className="details">
+        <label htmlFor='email' id='email-label'>Your Email : </label>
+        <input type='email' name='email' id='email' placeholder='Enter your email' />
+        <p id='slider_text'>How was your overall experience of the app?</p>
+        
+
+      </div>
+    </>
   )
 }
 
