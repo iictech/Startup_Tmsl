0ece72d9bee309552851dbf2d7e9a4986404f1ab
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-12-26 03:37:14 +0530

    Add feedback UI components and styles

--- src/Pages/Feedback/Feedback.js
+++ src/Pages/Feedback/Feedback.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import './Feedback.css'
+import feedbackgif from './Feedback.gif'
+import { Link } from 'react-router-dom'
+import SliderBar from './SliderBar'
 
 const Feedback = () => {
   return (
-    <div>Feedback</div>
+      <>
+        <div className="feedback-block" data-aos="fade-down">
+          <h1 className='feedback'>Feedback</h1> 
+          <img src={feedbackgif} alt='feedbackgif' className='feedbackgif' />
+        </div>
+        <div className="details" data-aos="fade-left">
+          <label htmlFor='email' className='text'>Your Email : </label>
+          <input type='email' name='email' id='email' placeholder='Enter your email' />
+          <p className='text'>How was your overall experience of the app?</p>
+          <SliderBar/>
+          <p className='text'>Suggestions</p>
+          <textarea name='suggestions' id='suggestions' cols='10' rows='20' placeholder='Enter your suggestions'></textarea>
+          <Link to="/thank-you" className='submit'>Submit</Link>
+        </div>
+      </>
   )
 }
 
