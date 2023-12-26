91cfc374c97fe93d9097a6c56c93967ae23d1cd6
Abhishikta Ray <127621443+Abhishikta03@users.noreply.github.com>
2023-12-27 00:32:41 +0530

    day 1

--- src/Pages/Account/Account.js
+++ src/Pages/Account/Account.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    
+    <>
+      <div className="account-box">
+          <h1>My Profile</h1>
+          <div className="account-image">
+              <input type="file" className="account-file-input" accept="image/*"></input>
+          </div>
+          <div className="account-details">
+              <h1>Rahul Sinha</h1>
+              <ul>
+                <li>Email     <b>rahul@gmail.com</b></li>
+                <li>Phone     <b>+91 9876543210</b></li>
+                <li>DOB     <b>01/01/1999</b></li>
+                <li>College     <b>Techno Main Salt Lake</b></li>
+              </ul>
+          </div>
+          <div className="account-buttons">
+              <a className="account-back-button">arrow</a>
+              <a className="account-edit-button">Edit</a>
+          </div>
+      </div>
+    </>
   )
 }
 
